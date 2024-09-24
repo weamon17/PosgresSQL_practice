084f674d25545e0f39519ac73385863eaba94cba
teetung17@gmail.com <teetung17@gmail.com>
2024-09-24 15:09:16 +0700

    FIND THE TOP SKILLS OF TOP PAYING-TOP

--- Projrct_PostgresSQL/TopPayingJobs.sql
+++ Projrct_PostgresSQL/TopPayingJobs.sql
@@ -13,7 +13,7 @@ SELECT
     job_posted_date,
     name as company_name
 FROM job_postings_fact
-RIGHT JOIN company_dim ON job_postings_fact.company_id = company_dim.company_id
+LEFT JOIN company_dim ON job_postings_fact.company_id = company_dim.company_id
 WHERE job_title_short = 'Data Analyst' AND 
 job_location = 'Anywhere' AND
 salary_year_avg IS NOT NULL
