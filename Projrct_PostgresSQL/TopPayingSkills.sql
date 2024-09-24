0160adfdf7457d66e6c9c21537835bf418a18a4c
teetung17@gmail.com <teetung17@gmail.com>
2024-09-24 15:33:51 +0700

    Update

--- Projrct_PostgresSQL/TopPayingSkills.sql
+++ Projrct_PostgresSQL/TopPayingSkills.sql
@@ -1,20 +1,27 @@
-WITH TopPayingJobs AS (
+WITH TopPayingJobs AS
+(
     SELECT 
         job_id,
         job_title,
-        job_location,
-        job_schedule_type,
-        job_posted_date,
-        name as company_name,
+        salary_year_avg,
+        name as company_name
     FROM job_postings_fact
     LEFT JOIN company_dim ON job_postings_fact.company_id = company_dim.company_id
-    WHERE job_title_short = 'Data Analyst' AND 
-    job_location = 'Anywhere' AND
-    salary_year_avg IS NOT NULL
-
+    WHERE 
+        job_title_short = 'Data Analyst' AND 
+        job_location = 'Anywhere' AND
+        salary_year_avg IS NOT NULL
     ORDER BY
         salary_year_avg DESC
     LIMIT 10
 )
 
-SELECT * FROM TopPayingJobs
+SELECT 
+    TopPayingJobs.*,
+    skills
+FROM TopPayingJobs
+INNER JOIN skills_job_dim  AS skills ON TopPayingJobs.job_id = skills.job_id
+INNER JOIN skills_dim on skills.skill_id = skills_dim.skill_id
+
+ORDER BY 
+    salary_year_avg DESC
